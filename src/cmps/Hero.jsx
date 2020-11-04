95dcac01f6e3eee22c80ba3f69567a7d4e3960ba
Asaf Dulman <asafdulman@gmail.com>
2020-11-04 11:33:37 +0200

    yuv

--- src/cmps/Hero.jsx
+++ src/cmps/Hero.jsx
@@ -12,7 +12,7 @@ export function Hero() {
         <div className="hero">
             <Animated animationIn="bounceInDown" animationOut="zoomOutDown" animationInDuration={1500} animationOutDuration={1000} isVisible={true}>
                 <div className="heading">
-                    <h2>Hello, I'm <span>Asaf Dulman.</span> </h2>
+                    <h2>Hello, I'm <span>Yuval Dulman.</span> </h2>
                     <h2>I'm a full-stack web developer</h2>
                     <button onClick={() => { scrollToPoint() }}>View my work <i className="fas fa-arrow-right arrow-right"></i></button>
                 </div>
